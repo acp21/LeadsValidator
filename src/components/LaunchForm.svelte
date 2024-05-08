5b10ef3c927d12face1f34dcdb7ff459eaf26d80
Adam Pohl <acpohl21@gmail.com>
2024-05-07 23:43:12 -0400

    Writing request building code

--- src/components/LaunchForm.svelte
+++ src/components/LaunchForm.svelte
@@ -1,29 +1,97 @@
 <script>
-    let inputColumns = [{ columnLetter: '', columnName: '' }];
-    let outputColumns = { websiteData: '', llmResponse: '' };
-    let llmInputs = [''];
+    // TODO: Add type safety to this
+    let directColumns = [{ columnLetter: '', columnName: '' }];
+    let customColumns = [{ columnLetter: '', columnName: '', type: '' }];
+    let promptColumns = [{prompt: '', outputColumnIndex: '', }];
+
+    const customTypes = ['website_body', 'website_url', 'user_input'];
   
+
+    function convertColumnLetterToIndex(columnLetter) {
+      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let index = 0;
+
+      for (let i = 0; i < columnLetter.length; i++) {
+        const char = columnLetter[i].toUpperCase();
+        const charIndex = alphabet.indexOf(char);
+        index = index * 26 + (charIndex + 1);
+      }
+
+      return index - 1;
+    }
     // Function to add a new input column
     function addInputColumn() {
-      inputColumns = [...inputColumns, { columnLetter: '', columnName: '' }];
+      directColumns = [...directColumns, { columnLetter: '', columnName: '' }];
     }
   
     // Function to add a new LLM input
     function addLLMInput() {
-      llmInputs = [...llmInputs, ''];
+      promptColumns = [...promptColumns, {prompt:'', outputColumnIndex: ''}];
+    }
+
+    function addCustomColumn() {
+      customColumns = [...customColumns, { columnLetter: '', columnName: '', type: ''}]
     }
   
     // Function to handle the form submission
     function submitForm() {
       // Process and handle the form data
-      console.log({ inputColumns, outputColumns, llmInputs });
+      buildRequest()
+      // console.log({ inputColumns, outputColumns, llmInputs });
+    }
+
+    function buildRequest() {
+      const request = {
+        direct_fields: [],
+        custom_fields: [],
+        prompt_fields: []
+      };
+
+      // Build direct_fields
+      directColumns.forEach((direct) => {
+        if (direct.columnLetter && direct.columnName) {
+          request.direct_fields.push({
+            field_properties: {
+              var_name: direct.columnName,
+              input_column_index: convertColumnLetterToIndex(direct.columnLetter)
+            }
+          });
+        }
+      });
+
+      // Build custom_fields
+      customColumns.forEach((custom) => {
+        if (custom.columnLetter && custom.columnName) {
+          request.direct_fields.push({
+            field_properties: {
+              var_name: custom.columnName,
+              input_column_index: convertColumnLetterToIndex(custom.columnLetter)
+            }
+          });
+        }
+      });
+
+      // Build prompt_fields
+      promptColumns.forEach((prompt, index) => {
+        if (prompt) {
+          request.prompt_fields.push({
+            output_col_index: directColumns.length + (customColumns.websiteData ? 1 : 0),
+            output_var_name: `llmResponse${index + 1}`,
+            order: index,
+            prompt: {
+              base_text: prompt
+            }
+          });
+        }
+      });
+      console.log(request)
     }
   </script>
   
   <form on:submit|preventDefault={submitForm}>
     <!-- Input Columns -->
     <h3>Input Columns</h3>
-    {#each inputColumns as input, index}
+    {#each directColumns as input, index}
       <div class="input-group">
         <input
           type="text"
@@ -35,37 +103,53 @@
           placeholder="Column Name"
           bind:value={input.columnName}
         />
-        {#if index === inputColumns.length - 1}
+        {#if index === directColumns.length - 1}
           <button type="button" on:click={addInputColumn}>+</button>
         {/if}
       </div>
     {/each}
   
     <!-- Output Columns -->
-    <h3>Output Columns</h3>
-    <div class="input-group">
-      <input
-        type="text"
-        placeholder="Website Data Output Column"
-        bind:value={outputColumns.websiteData}
-      />
-      <input
-        type="text"
-        placeholder="LLM Response Output Column"
-        bind:value={outputColumns.llmResponse}
-      />
-    </div>
+    <h3>Custom Fields</h3>
+    {#each customColumns as input, index}
+      <div class="input-group">
+        <input
+          type="text"
+          placeholder="Custom Field Column"
+          bind:value={input.columnLetter}
+        />
+        <input
+          type="text"
+          placeholder="Custom Variable Name"
+          bind:value={input.columnName}
+        />
+        <select bind:value={input.type}>
+          <option value="">Select Type</option>
+          {#each customTypes as type}
+            <option value={type}>{type}</option>
+          {/each}
+        </select>
+        {#if index === directColumns.length - 1}
+          <button type="button" on:click={addCustomColumn}>+</button>
+        {/if}
+      </div>
+    {/each}
   
     <!-- LLM Inputs -->
     <h3>LLM Inputs</h3>
-    {#each llmInputs as input, index}
+    {#each promptColumns as input, index}
       <div class="input-group">
         <input
           type="text"
           placeholder="LLM Input"
-          bind:value={llmInputs[index]}
+          bind:value={promptColumns[index]}
         />
-        {#if index === llmInputs.length - 1}
+        <input
+          type="text"
+          placeholder="Output Column Index"
+          bind:value={promptColumns.outputColumnIndex}
+        />
+        {#if index === promptColumns.length - 1}
           <button type="button" on:click={addLLMInput}>+</button>
         {/if}
       </div>
